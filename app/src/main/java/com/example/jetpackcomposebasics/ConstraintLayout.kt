8f77f38e7918c38b6b5bfc8bca151b183d1c76b7
Ivan <icarpiodeveloper@gmail.com>
2024-08-14 20:00:10 +0200

    TextFields

--- app/src/main/java/com/example/jetpackcomposebasics/ConstraintLayout.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/ConstraintLayout.kt
@@ -1,10 +1,28 @@
 package com.example.jetpackcomposebasics
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,5 +57,164 @@ fun ConstraintExample(){
             start.linkTo(boxRed.end)
             }){
         }
+    }
+}
+
+@Composable
+fun MyBox(name: String) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .width(100.dp)
+                .height(100.dp)
+                .background(Color.Green)
+                .verticalScroll(
+                    rememberScrollState()
+                ), contentAlignment = Alignment.BottomCenter) {
+            Text(text = name) // Add some content to the inner Box
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun MyColumn(){
+    Column(
+        Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()), verticalArrangement = Arrangement.SpaceBetween) {
+        Text("Ejemplo1",
+            Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .height(100.dp))
+        Text(text = "Ejemplo2",
+            Modifier
+                .background(Color.Blue)
+                .fillMaxWidth()
+                .height(100.dp))
+        Text(text = "Ejemplo3",
+            Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(100.dp))
+        Text(text = "Ejemplo3",
+            Modifier
+                .background(Color.Magenta)
+                .fillMaxWidth()
+                .height(100.dp))
+        Text("Ejemplo1",
+            Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .height(100.dp))
+        Text(text = "Ejemplo2",
+            Modifier
+                .background(Color.Blue)
+                .fillMaxWidth()
+                .height(100.dp))
+        Text(text = "Ejemplo3",
+            Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(100.dp))
+        Text(text = "Ejemplo3",
+            Modifier
+                .background(Color.Magenta)
+                .fillMaxWidth()
+                .height(100.dp))
+    }
+}
+
+
+@Composable
+fun MyRow(){
+    Row(
+        Modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState())) {
+        Text("Ejemplo1",
+            Modifier
+                .background(Color.Red)
+                .width(100.dp))
+        Text(text = "Ejemplo2",
+            Modifier
+                .background(Color.Blue)
+                .width(100.dp))
+        Text(text = "Ejemplo3",
+            Modifier
+                .background(Color.Cyan)
+                .width(100.dp))
+        Text(text = "Ejemplo4",
+            Modifier
+                .background(Color.Cyan)
+                .width(100.dp))
+        Text(text = "Ejemplo4",
+            Modifier
+                .background(Color.Magenta)
+                .width(100.dp))
+    }
+}
+
+@Composable
+fun MyComplexLayout() {
+    Column(Modifier.fillMaxSize()) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Red), contentAlignment = Alignment.TopCenter){
+            Text(text = "Texto 1")
+        }
+        MySpacer(size = 30)
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)){
+            Box(
+                Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color.Green), contentAlignment = Alignment.Center){
+                Text(text = "Texto 2")
+            }
+            Box(
+                Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color.Magenta), contentAlignment = Alignment.Center){
+                Text(text = "Texto 3")
+            }
+        }
+        MySpacer(size = 30)
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Yellow), contentAlignment = Alignment.BottomCenter){
+            Text(text = "Texto 4")
+        }
+    }
+}
+
+@Composable
+fun MySpacer(size:Int){
+    Spacer(modifier = Modifier.height(size.dp))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyStateExample(){
+    var counter by rememberSaveable { mutableIntStateOf(0) }
+    Column(Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(onClick = { counter += 1 }) {
+            Text(text = "+")
+        }
+        Button(onClick = { counter -= 1 }) {
+            Text(text = "-")
+        }
+        Text(text = "He sido pulsado $counter veces")
     }
 }
--- app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -4,31 +4,37 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
+
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -37,154 +43,77 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeBasicsTheme {
                 //window.statusBarColor = Color.Magenta.toArgb()
-                window.statusBarColor = MaterialTheme.colorScheme.primary.toArgb() // Cambiar el color de la barra de estado
-                Scaffold(modifier = Modifier.fillMaxSize(),
+                window.statusBarColor =
+                    MaterialTheme.colorScheme.primary.toArgb() // Cambiar el color de la barra de estado
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
                 ) {
-
+                    //MyText()
+                    var myTextParams by remember { mutableStateOf("") }
+                    MyTextWithParameters(myTextParams) { myTextParams = it }
                 }
             }
         }
     }
 }
-@Composable
-fun MyBox(name: String) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Box(
-            modifier = Modifier
-                .width(100.dp)
-                .height(100.dp)
-                .background(Color.Green)
-                .verticalScroll(
-                    rememberScrollState()
-                ), contentAlignment = Alignment.BottomCenter) {
-            Text(text = name) // Add some content to the inner Box
-        }
-    }
-}
 
-//@Preview(showBackground = true)
-@Composable
-fun MyColumn(){
-    Column(
-        Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()), verticalArrangement = Arrangement.SpaceBetween) {
-        Text("Ejemplo1",
-            Modifier
-                .background(Color.Red)
-                .fillMaxWidth()
-                .height(100.dp))
-        Text(text = "Ejemplo2",
-            Modifier
-                .background(Color.Blue)
-                .fillMaxWidth()
-                .height(100.dp))
-        Text(text = "Ejemplo3",
-            Modifier
-                .background(Color.Cyan)
-                .fillMaxWidth()
-                .height(100.dp))
-        Text(text = "Ejemplo3",
-            Modifier
-                .background(Color.Magenta)
-                .fillMaxWidth()
-                .height(100.dp))
-        Text("Ejemplo1",
-            Modifier
-                .background(Color.Red)
-                .fillMaxWidth()
-                .height(100.dp))
-        Text(text = "Ejemplo2",
-            Modifier
-                .background(Color.Blue)
-                .fillMaxWidth()
-                .height(100.dp))
-        Text(text = "Ejemplo3",
-            Modifier
-                .background(Color.Cyan)
-                .fillMaxWidth()
-                .height(100.dp))
-        Text(text = "Ejemplo3",
-            Modifier
-                .background(Color.Magenta)
-                .fillMaxWidth()
-                .height(100.dp))
-    }
-}
-
-
-@Composable
-fun MyRow(){
-    Row(
-        Modifier
-            .fillMaxSize()
-            .horizontalScroll(rememberScrollState())) {
-        Text("Ejemplo1",
-            Modifier
-                .background(Color.Red)
-                .width(100.dp))
-        Text(text = "Ejemplo2",
-            Modifier
-                .background(Color.Blue)
-                .width(100.dp))
-        Text(text = "Ejemplo3",
-            Modifier
-                .background(Color.Cyan)
-                .width(100.dp))
-        Text(text = "Ejemplo4",
-            Modifier
-                .background(Color.Cyan)
-                .width(100.dp))
-        Text(text = "Ejemplo4",
-            Modifier
-                .background(Color.Magenta)
-                .width(100.dp))
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
-fun MyComplexLayout() {
-    Column(Modifier.fillMaxSize()) {
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .weight(1f)
-                .background(Color.Red), contentAlignment = Alignment.TopCenter){
-            Text(text = "Texto 1")
-        }
-        MySpacer(size = 30)
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .weight(1f)){
-            Box(
-                Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .background(Color.Green), contentAlignment = Alignment.Center){
-                Text(text = "Texto 2")
-            }
-            Box(
-                Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .background(Color.Magenta), contentAlignment = Alignment.Center){
-                Text(text = "Texto 3")
-            }
-        }
-        MySpacer(size = 30)
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .weight(1f)
-                .background(Color.Yellow), contentAlignment = Alignment.BottomCenter){
-            Text(text = "Texto 4")
-        }
+fun MyText() {
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Esto es un ejemplo")
+        Text(text = "Esto es un ejemplo", color = colorResource(id = R.color.teal_700))
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.ExtraBold)
+        Text(text = "Esto es un ejemplo", fontFamily = FontFamily.Cursive)
+        Text(text = "Esto es un ejemplo", textDecoration = TextDecoration.Underline)
+        Text(
+            text = "Esto es un ejemplo", style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(TextDecoration.Underline, TextDecoration.LineThrough)
+                )
+            )
+        )
+        Text(text = "Esto es un ejemplo", fontSize = 30.sp)
+
+        //TextField
+        var myText by remember { mutableStateOf("") }
+        TextField(value = myText, onValueChange = { myText = it })
+
+        //TextFieldAdavance
+        var myTextAd by remember { mutableStateOf("") }
+        TextField(
+            value = myTextAd,
+            onValueChange = {
+                myTextAd =
+                    if (it.contains("a")) {
+                        it.replace("a", "")
+                    } else {
+                        it
+                    }
+            },
+            label = { Text(text = "Introduce un texto") })
+
+        //Outlined Text
+        var myOutlined by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = myOutlined,
+            onValueChange = { myOutlined = it },
+            Modifier.padding(24.dp),
+            label = { Text(text = "Text Outlined")},
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Red,
+                unfocusedBorderColor = Color.Blue
+            )
+        )
     }
 }
 
 @Composable
-fun MySpacer(size:Int){
-    Spacer(modifier = Modifier.height(size.dp))
+fun MyTextWithParameters(name:String, onValueChanged:(String) -> Unit) {
+    TextField(value = name, onValueChange = {onValueChanged(it)})
 }
