e363ffc50537627508a2d0c1a0e2cd7e435b48b7
Ivan <icarpiodeveloper@gmail.com>
2024-08-15 11:04:09 +0200

    Buttons, Image, ProgressBar

--- app/src/main/java/com/example/jetpackcomposebasics/ConstraintLayout.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/ConstraintLayout.kt
@@ -1,6 +1,9 @@
 package com.example.jetpackcomposebasics
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,22 +14,44 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 
 @Preview(showBackground = true)
@@ -217,4 +242,179 @@ fun MyStateExample(){
         }
         Text(text = "He sido pulsado $counter veces")
     }
+}
+
+@Composable
+fun MyText() {
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Esto es un ejemplo")
+        Text(text = "Esto es un ejemplo", color = colorResource(id = R.color.teal_700))
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.ExtraBold)
+        Text(text = "Esto es un ejemplo", fontFamily = FontFamily.Cursive)
+        Text(text = "Esto es un ejemplo", textDecoration = TextDecoration.Underline)
+        Text(
+            text = "Esto es un ejemplo", style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(TextDecoration.Underline, TextDecoration.LineThrough)
+                )
+            )
+        )
+        Text(text = "Esto es un ejemplo", fontSize = 30.sp)
+
+        //TextField
+        var myText by remember { mutableStateOf("") }
+        TextField(value = myText, onValueChange = { myText = it })
+
+        //TextFieldAdavance
+        var myTextAd by remember { mutableStateOf("") }
+        TextField(
+            value = myTextAd,
+            onValueChange = {
+                myTextAd =
+                    if (it.contains("a")) {
+                        it.replace("a", "")
+                    } else {
+                        it
+                    }
+            },
+            label = { Text(text = "Introduce un texto") })
+
+        //Outlined Text
+        var myOutlined by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = myOutlined,
+            onValueChange = { myOutlined = it },
+            Modifier.padding(24.dp),
+            label = { Text(text = "Text Outlined")},
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Red,
+                unfocusedBorderColor = Color.Blue
+            )
+        )
+    }
+}
+
+@Composable
+fun MyTextWithParameters(name:String, onValueChanged:(String) -> Unit) {
+    TextField(value = name, onValueChange = {onValueChanged(it)})
+}
+
+@Composable
+fun MyButton(){
+    var enabled by rememberSaveable { mutableStateOf(true) }
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(24.dp)) {
+        Button(
+            onClick = {enabled = false },
+            enabled = enabled,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue, // Cambia el color de fondo del botón
+                contentColor = Color.White   // Cambia el color del contenido (texto o íconos)
+            ),
+            border = BorderStroke(5.dp, Color.Yellow)
+        ) {
+            Text(text = "Click")
+        }
+
+        //Outlined Button
+        OutlinedButton(
+            onClick = {},
+            Modifier.padding(top = 8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Red,
+                contentColor = Color.White,
+                disabledContainerColor = Color.Gray,  // Fondo cuando el botón está deshabilitado
+                disabledContentColor = Color.DarkGray, // Texto cuando el botón está deshabilitado
+            ),
+            border = BorderStroke(2.dp, Color.Yellow), // Borde rojo
+
+        ) {
+            Text(text = "Click Outlined")
+        }
+
+        //Text Button
+        TextButton(onClick = {  }) {
+            Text(text = "Text Button")
+        }
+    }
+}
+
+// Imágenes apiladas verticalmente (una sobre la otra)
+@Composable
+fun MyImages() {
+    Column(
+        modifier = Modifier.fillMaxSize(), // Llenar toda la pantalla
+        horizontalAlignment = Alignment.CenterHorizontally, // Centrar horizontalmente
+        verticalArrangement = Arrangement.Center // Centrar verticalmente
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.image),
+            contentDescription = "Imagen 1",
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.CenterHorizontally),
+            contentScale = ContentScale.Crop
+        )
+        Image(
+            painter = painterResource(id = R.drawable.image),
+            contentDescription = "Imagen 2",
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.CenterHorizontally),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+//Imágenes colocadas horizontalmente (una al lado de la otra)
+@Composable
+fun MyImages2() {
+    Row(
+        modifier = Modifier.fillMaxSize(), // Llenar toda la pantalla
+        horizontalArrangement = Arrangement.Center, // Centrar horizontalmente
+        verticalAlignment = Alignment.CenterVertically // Centrar verticalmente
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.image),
+            contentDescription = "Imagen 1",
+            modifier = Modifier
+                .padding(16.dp)
+                .weight(1f), // Tomar espacio proporcional
+            contentScale = ContentScale.Crop
+        )
+        Image(
+            painter = painterResource(id = R.drawable.image),
+            contentDescription = "Imagen 2",
+            modifier = Modifier
+                .padding(16.dp)
+                .weight(1f), // Tomar espacio proporcional
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Composable
+fun MyIcon(){
+    Icon(contentDescription = "Image Icon", imageVector = Icons.Rounded.Star, tint = Color.Blue)
+}
+@Composable
+fun MyImageCircle(){
+    Image(
+        painter = painterResource(id = R.drawable.image),
+        contentDescription = "Imagen 1",
+        modifier = Modifier
+            .padding(16.dp)
+            .clip(
+                //RoundedCornerShape(50f)
+                CircleShape
+            )
+            .border(30.dp, Color.Red, CircleShape),
+        contentScale = ContentScale.Crop
+    )
 }
--- app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -1,38 +1,45 @@
 package com.example.jetpackcomposebasics
-
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
 
 
@@ -43,14 +50,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeBasicsTheme {
                 //window.statusBarColor = Color.Magenta.toArgb()
-                window.statusBarColor =
-                    MaterialTheme.colorScheme.primary.toArgb() // Cambiar el color de la barra de estado
+                window.statusBarColor = MaterialTheme.colorScheme.primary.toArgb() // Cambiar el color de la barra de estado
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    //MyText()
-                    var myTextParams by remember { mutableStateOf("") }
-                    MyTextWithParameters(myTextParams) { myTextParams = it }
+                    //var myTextParams by remember { mutableStateOf("") }
+                    //MyTextWithParameters(myTextParams) { myTextParams = it }
+                    MyProgressBarAd()
                 }
             }
         }
@@ -58,62 +64,54 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Preview(showBackground = true)
+
 @Composable
-fun MyText() {
+fun MyProgressBar(){
+    var showLoading by rememberSaveable { mutableStateOf(false) }
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if(showLoading) {
+            CircularProgressIndicator(color = Color.Red)
+            LinearProgressIndicator(Modifier.padding(top = 32.dp), color = Color.Red, trackColor = Color.Yellow)
+        }
+        Button(onClick = { showLoading = !showLoading}) {
+            Text(text = "Load")
+        }
+
+    }
+}
+@Composable
+fun MyProgressBarAd(){
+    var progressStatus by rememberSaveable { mutableFloatStateOf(0f) }
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Esto es un ejemplo")
-        Text(text = "Esto es un ejemplo", color = colorResource(id = R.color.teal_700))
-        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.ExtraBold)
-        Text(text = "Esto es un ejemplo", fontFamily = FontFamily.Cursive)
-        Text(text = "Esto es un ejemplo", textDecoration = TextDecoration.Underline)
-        Text(
-            text = "Esto es un ejemplo", style = TextStyle(
-                textDecoration = TextDecoration.combine(
-                    listOf(TextDecoration.Underline, TextDecoration.LineThrough)
-                )
-            )
-        )
-        Text(text = "Esto es un ejemplo", fontSize = 30.sp)
+        CircularProgressIndicator(color = Color.Red, progress = progressStatus)
 
-        //TextField
-        var myText by remember { mutableStateOf("") }
-        TextField(value = myText, onValueChange = { myText = it })
-
-        //TextFieldAdavance
-        var myTextAd by remember { mutableStateOf("") }
-        TextField(
-            value = myTextAd,
-            onValueChange = {
-                myTextAd =
-                    if (it.contains("a")) {
-                        it.replace("a", "")
-                    } else {
-                        it
-                    }
-            },
-            label = { Text(text = "Introduce un texto") })
-
-        //Outlined Text
-        var myOutlined by remember { mutableStateOf("") }
-        OutlinedTextField(
-            value = myOutlined,
-            onValueChange = { myOutlined = it },
-            Modifier.padding(24.dp),
-            label = { Text(text = "Text Outlined")},
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Color.Red,
-                unfocusedBorderColor = Color.Blue
-            )
-        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp), // Agrega padding para mayor estética
+            horizontalArrangement = Arrangement.Center, // Centra los botones horizontalmente
+            verticalAlignment = Alignment.CenterVertically // Opcional: centrar verticalmente si es necesario
+        ) {
+            Button(onClick = { progressStatus += 0.1f }) {
+                Text(text = "Incrementar")
+            }
+            Button(
+                onClick = { progressStatus -= 0.1f },
+                modifier = Modifier.padding(start = 8.dp) // Agrega espacio entre los botones
+            ) {
+                Text(text = "Reducir")
+            }
+        }
+        //LinearProgressIndicator(Modifier.padding(top = 32.dp), color = Color.Red, trackColor = Color.Yellow)
     }
-}
-
-@Composable
-fun MyTextWithParameters(name:String, onValueChanged:(String) -> Unit) {
-    TextField(value = name, onValueChange = {onValueChanged(it)})
 }
